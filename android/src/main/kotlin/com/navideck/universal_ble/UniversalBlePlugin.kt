1f8e771ea017b139d47ba0672c6635fe98b8220e
Johan Jacobs <johanjcbs@gmail.com>
2024-12-09 19:57:51 +0200

    Enable Android auto connect.

--- android/src/main/kotlin/com/navideck/universal_ble/UniversalBlePlugin.kt
+++ android/src/main/kotlin/com/navideck/universal_ble/UniversalBlePlugin.kt
@@ -203,12 +203,12 @@ class UniversalBlePlugin : UniversalBlePlatformChannel, BluetoothGattCallback(),
         val gatt = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             remoteDevice.connectGatt(
                 context,
-                false,
+                true,
                 this,
                 BluetoothDevice.TRANSPORT_LE
             )
         } else {
-            remoteDevice.connectGatt(context, false, this)
+            remoteDevice.connectGatt(context, true, this)
         }
         knownGatts.add(gatt)
     }
